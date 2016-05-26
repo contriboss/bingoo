dbc720f41e783ef14ce465e38b637d4852e692ce
Radhames Brito <rbritom@gmail.com>
2016-05-25 20:51:17 -0400

    initial commit

---
+++ lib/bingoo.rb
@@ -0,0 +1,5 @@
+require "bingoo/version"
+
+module Bingoo
+  # Your code goes here...
+end
---
+++ spec/bingoo_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Bingoo do
+  describe '.configure' do
+    it 'Sets global configuration attributes' do
+      described_class.configure do |config|
+        config.username = 'test username'
+        config.password = '123456'
+        config.developer_token = '98765432'
+      end
+
+      expect(described_class.config.username).to eq('test username')
+      expect(described_class.config.password).to eq('123456')
+      expect(described_class.config.developer_token).to eq('98765432')
+    end
+  end
+end
